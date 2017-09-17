71c30e36ea8beb2391831e2cff0d4e1e6ab3471c
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-09-17 16:27:17 +0300

    add React Router

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import todoApp from './reducers';
-import App from './components/App';
-import { loadState, saveState } from './localStorage';
-import throttle from 'lodash/throttle';
+import configureStore from './configureStore';
+import Root from './components/Root';
 
-const persistedState = loadState();
+const store = configureStore();
 
-const store = createStore(todoApp, persistedState);
-
-store.subscribe(
-  throttle(() => {
-    saveState({
-      todos: store.getState().todos,
-    });
-  }, 1000)
-);
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+render(<Root store={store} />, document.getElementById('root'));
